76ed4ffbb25603b04b8e53000e202fe1fafcde26
ibekkouc <inas.bekkouche@capgemini.com>
2022-07-24 12:28:40 +0200

    ADD properties

--- src/main/java/com/instacodeApp/InstacodeAppApplication.java
+++ src/main/java/com/instacodeApp/InstacodeAppApplication.java
@@ -2,11 +2,17 @@ package com.instacodeApp;
 
 import com.instacodeApp.use_cases.authentication.role.domain.Role;
 import com.instacodeApp.use_cases.authentication.role.repository.RoleRepository;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -18,6 +24,35 @@ public class InstacodeAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstacodeAppApplication.class, args);
+	}
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// Add HTTP to HTTPS redirect
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+		return tomcat;
+	}
+
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8081);
+		connector.setSecure(false);
+		connector.setRedirectPort(8445);
+		return connector;
 	}
 
 
