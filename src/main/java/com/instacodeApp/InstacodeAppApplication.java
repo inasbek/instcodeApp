7a1b41b044f1cfa2defc510ec7ac172a49eb1045
ibekkouc <inas.bekkouche@capgemini.com>
2022-07-24 12:47:55 +0200

    old version to deploy

--- src/main/java/com/instacodeApp/InstacodeAppApplication.java
+++ src/main/java/com/instacodeApp/InstacodeAppApplication.java
@@ -25,35 +25,6 @@ public class InstacodeAppApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(InstacodeAppApplication.class, args);
 	}
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		// Add HTTP to HTTPS redirect
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-		return tomcat;
-	}
-
-	private Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8081);
-		connector.setSecure(false);
-		connector.setRedirectPort(8445);
-		return connector;
-	}
 
 
 	@Autowired
